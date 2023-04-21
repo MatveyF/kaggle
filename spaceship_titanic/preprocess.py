2bc7358ac57992e5160afebd88c7cff381b9406c
matthew.f <matthew.fedoseev@turintech.ai>
2023-04-21 11:23:26 +0100

    fitting simple catboost

--- spaceship_titanic/preprocess.py
+++ spaceship_titanic/preprocess.py
@@ -1,5 +1,6 @@
 # Imports
 import argparse
+from pathlib import Path
 
 import pandas as pd
 import numpy as np
@@ -35,7 +36,8 @@ def _clean_features(df: pd.DataFrame) -> pd.DataFrame:
     df["CryoSleep"] = df["CryoSleep"].fillna(False)
 
     df.drop(columns=["cabin_num"], inplace=True)
-    df["cabin_side"] = df["cabin_side"].fillna(False)
+
+    df["cabin_side"] = df["cabin_side"].fillna(True)
 
     return df
 
@@ -61,12 +63,37 @@ def _extract_features(df: pd.DataFrame) -> pd.DataFrame:
     return df
 
 
+def main(input_path: Path, output_path: Path):
+    orig_train = pd.read_csv(input_path / "train.csv")
+    orig_test = pd.read_csv(input_path / "test.csv")
+
+    train = preprocess_data(orig_train)
+    test = preprocess_data(orig_test)
+
+    from catboost import CatBoostClassifier
+
+    y = train["Transported"]
+    X = train.drop(columns=["Transported"])
+
+    model = CatBoostClassifier()
+    model.fit(X, y)
+
+    y_pred = model.predict(test)
+
+    result = pd.DataFrame({"PassengerId": orig_test["PassengerId"], "Transported": y_pred})
+
+    # result["Transported"] = result["Transported"].apply(lambda x: x in [1, True])
+
+    result.to_csv("submission.csv", index=False)
+
+    # to try:
+    #  1. cross validation
+    #  2. model tuning
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', type=str, required=True)
     parser.add_argument('-o', type=str, required=True)
     args = parser.parse_args()
 
-    data = pd.read_csv(args.i)
-    data = preprocess_data(data)
-    data.to_csv(args.o, index=False)
+    main(Path(args.i), Path(args.o))
